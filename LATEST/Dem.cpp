d8bde95c9b77aa7df30e99666ca9600f43bb5c7f
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:50:58 +0200

    Update: AUTOSAR basic software service module Dem

--- LATEST/Dem.cpp
+++ LATEST/Dem.cpp
@@ -100,8 +100,14 @@ FUNC(void, DEM_CODE) module_Dem::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_Dem as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == Dem_InitCheck)
