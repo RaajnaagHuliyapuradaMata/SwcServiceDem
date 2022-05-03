5d01a86e3840bba18ea34cfe76e9068c11134dc8
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:09:01 +0000

    Update: AUTOSAR basic software service module Dem

--- LATEST/Dem.cpp
+++ LATEST/Dem.cpp
@@ -86,79 +86,79 @@ FUNC(void, DEM_CODE) module_Dem::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, DEM_CONFIG_DATA, DEM_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == Dem_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == Dem_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == Dem_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgDem;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == Dem_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == Dem_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == Dem_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, DEM_CODE) module_Dem::DeInitFunction(void){
 #if(STD_ON == Dem_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == Dem_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == Dem_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == Dem_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == Dem_InitCheck)
    }
 #endif
 }
 
 FUNC(void, DEM_CODE) module_Dem::MainFunction(void){
 #if(STD_ON == Dem_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == Dem_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == Dem_InitCheck)
+   }
+   else{
+#if(STD_ON == Dem_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
