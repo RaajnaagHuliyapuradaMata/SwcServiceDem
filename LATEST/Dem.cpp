fbc09f5ce8298fba5763c8a953ed501c55e6c6b4
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-07-26 19:51:11 +0200

    Update: AUTOSAR basic software service module for diagnostic event manager

--- LATEST/Dem.cpp
+++ LATEST/Dem.cpp
@@ -61,7 +61,7 @@ FUNC(void, DEM_CODE) module_Dem::InitFunction(
             (NULL_PTR != lptrConstModule)
          && (NULL_PTR != lptrCfgModule)
       ){
-         lptrConst = lptrConstModule;
+         lptrConst = (const ConstDem_Type*)lptrConstModule;
          lptrCfg   = lptrCfgModule;
       }
       else{
