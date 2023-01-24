3af4da336e3583853f4505e857b2ee51a7e3a3eb
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:50:53 +0100

    Update: AUTOSAR basic software service module for diagnostic event manager

---
+++ main_R2023/infServiceDem_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceDem_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEDEM_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTSERVICEDEM_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstServiceDem                                                                              \
+   ,  {CONSTSERVICEDEM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEDEM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
