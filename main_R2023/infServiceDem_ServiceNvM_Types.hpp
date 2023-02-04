14c776290291ecacf46b90ce2ca391e88cab497e
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:44 +0100

    Update: AUTOSAR basic software service module for diagnostic event manager

--- main_R2023/infServiceDem_ServiceNvM_Types.hpp
+++ main_R2023/infServiceDem_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceDem_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceDem_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
