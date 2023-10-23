920d71ab05b92e530462bf77d604e8bb34b23ea7
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-23 08:01:24 +0200

    AUTOSAR - Hotfixes - SwcServiceDem - pIntegrationRdcXnf_cB_LATEST

---
+++ pIntegrationRdcXnf_cB_LATEST/Types_SwcServiceDem.hpp
@@ -0,0 +1,63 @@
+#pragma once
+/******************************************************************************/
+/* File   : Types_SwcServiceDem.hpp                                           */
+/*                                                                            */
+/* Author : Raajnaag HULIYAPURADA MATA                                        */
+/*                                                                            */
+/* License / Warranty / Terms and Conditions                                  */
+/*                                                                            */
+/* Everyone is permitted to copy and distribute verbatim copies of this lice- */
+/* nse document, but changing it is not allowed. This is a free, copyright l- */
+/* icense for software and other kinds of works. By contrast, this license is */
+/* intended to guarantee your freedom to share and change all versions of a   */
+/* program, to make sure it remains free software for all its users. You have */
+/* certain responsibilities, if you distribute copies of the software, or if  */
+/* you modify it: responsibilities to respect the freedom of others.          */
+/*                                                                            */
+/* All rights reserved. Copyright � 1982 Raajnaag HULIYAPURADA MATA           */
+/*                                                                            */
+/* Always refer latest software version from:                                 */
+/* https://github.com/RaajnaagHuliyapuradaMata?tab=repositories               */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+typedef enum{
+      SwcServiceDem_eIdDtc_NONE
+   ,  SwcServiceDem_eIdDtc_MAX
+}Type_SwcServiceDem_eIdDtc;
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ pIntegrationRdcXnf_cB_LATEST/b_git_push.sh
@@ -0,0 +1,4 @@
+git add .
+git status
+git commit -m "AUTOSAR - Hotfixes - SwcServiceDem - pIntegrationRdcXnf_cB_LATEST"
+git push
