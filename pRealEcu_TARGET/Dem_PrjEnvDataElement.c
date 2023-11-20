eb14dd312b22c854b6705c5a92fde1748b3368b2
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-20 16:23:09 +0100

    AUTOSAR Migration Activities

--- pRealEcu_TARGET/Dem_PrjEnvDataElement.c
+++
@@ -1,7 +0,0 @@
-#include "Std_Types.hpp"
-
-#include "Dem_Lok_InternalAPI.hpp"
-
-Std_ReturnType Dem_ReadPrjInternalDataElement_0(uint8* Buffer, const Dem_InternalEnvData* internalData){
-    return E_NOT_OK;
-}
