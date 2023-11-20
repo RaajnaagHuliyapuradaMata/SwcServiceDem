cd0622ae068c219895b22959d51f52ec05dc9dbf
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 08:59:45 +0100

    AUTOSAR Migration Activities

--- pRealEcu_LATEST/Dem_ClientClearMachine.c
+++
@@ -1,9 +0,0 @@
-#include "Std_Types.hpp"
-
-#include "Dem_ClientClearMachine.hpp"
-
-#define DEM_START_SEC_RAM_CLEARED
-#include "Dem_Cfg_MemMap.hpp"
-Dem_ClientClearMachineType Dem_ClientClearMachine;
-#define DEM_STOP_SEC_RAM_CLEARED
-#include "Dem_Cfg_MemMap.hpp"
--- pRealEcu_LATEST/Dem_DebSharing.hpp
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef DEM_DEBSHARING_H
-#define DEM_DEBSHARING_H
-
-#define DEM_DEB_LOCAL_CALC_PARAMS
-
-#endif
-
